3f82172f28fe174683fb5e629ae361aa9ebc20b8
Bree <breanna.sowle@kearsarge.org>
2014-11-20 09:03:59 -0500

    Changed "Daemon" to "Demon" and replaced Ranged with Leviathan code

--- Leviathan.py
+++ Leviathan.py
@@ -2,3 +2,12 @@ import pygame, math
 from Demon import Demon
 
 class Leviathan(Demon):
+	def __init__():
+	self.upImages = [pygame.image.load("RSC/Leviathan/lUp1.png"),
+						pygame.image.load("RSC/Leviathan/lUp2.png")]
+	self.downImages = [pygame.image.load("RSC/Leviathan/lDown1.png"),
+						pygame.image.load("RSC/Leviathan/lDown2.png")]
+	self.leftImages = [pygame.image.load("RSC/Leviathan/lLeft1.png"),
+						pygame.image.load("RSC/Leviathan/lLeft2.png")]
+	self.rightImages = [pygame.image.load("RSC/Leviathan/lRight1.png"),
+						pygame.image.load("RSC/Leviathan/lRight2.png")]
