ea5d656b241c4a3126ad9b1addafb2857ea8a614
Bree <breanna.sowle@kearsarge.org>
2014-11-20 09:02:42 -0500

    Filler images for demon, player, leiathan, and ghost added to their RSC files

--- Demons.py
+++ Demons.py
@@ -2,4 +2,12 @@ import pygame, math
 from Creature import Creature
 
 class Demon(Creature):
-	(self, pos, speed = [0,0])
+	def __init__(self, pos, speed = [0,0]):
+	self.upImages = [pygame.image.load("RSC/Demon/dUp1.png"),
+						pygame.image.load("RSC/Demon/dUp2.png")]
+	self.downImages = [pygame.image.load("RSC/Demon/dDown1.png"),
+						pygame.image.load("RSC/Demon/dDown2.png")]
+	self.leftImages = [pygame.image.load("RSC/Demon/dLeft1.png"),
+						pygame.image.load("RSC/Demon/dLeft2.png")]
+	self.rightImages = [pygame.image.load("RSC/Demon/dRight1.png"),
+						pygame.image.load("RSC/Demon/dRight2.png")]
--- Ghost.py
+++ Ghost.py
@@ -2,3 +2,12 @@ import pygame
 from Creature import Creature
 
 class Ghost(Creature):
+	def __init__():
+	self.upImages = [pygame.image.load("RSC/Ghost/gUp1.png"),
+						pygame.image.load("RSC/Ghost/gUp2.png")]
+	self.downImages = [pygame.image.load("RSC/Ghost/gDown1.png"),
+						pygame.image.load("RSC/Ghost/gDown2.png")]
+	self.leftImages = [pygame.image.load("RSC/Ghost/gLeft1.png"),
+						pygame.image.load("RSC/Ghost/gLeft2.png")]
+	self.rightImages = [pygame.image.load("RSC/Ghost/gRight1.png"),
+						pygame.image.load("RSC/Ghost/gRight2.png")]
