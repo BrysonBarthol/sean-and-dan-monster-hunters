339db31aa08b41c337974c4bf57c08a094b832e9
Bryson Barthol <bryson.barthol@kearsarge.org>
2014-11-21 09:35:34 -0500

    Started Maps

--- Game.py
+++ Game.py
@@ -3,3 +3,21 @@ from Block import Block
 from Deamon import Deamon
 from Ghost import Ghost
 from Leviathan import Leviathan
+from Level import Level
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 1000 
+height = 700
+size = width, height
+
+bgColor = r,g,b = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+screenCol, screenRow = 2,4
+screenMaxCol, screenMaxRow = 5, 7
+
+level = Level("screen"+ str(screenCol) + str(screenRow))
--- Level.py
+++ Level.py
@@ -1,1 +1,72 @@
+import pygame, math, sys, time
+from Block import Block
 
+class Level():
+    def __init__(self, level, screenSize):
+        self.screenSize = screenSize
+        self.screenWidth = screenSize[0]
+        self.screenHeight = screenSize[1]
+        self.blocks = []
+        self.enemies = []
+        self.player
+        self.load(level)
+        self.blockSize = 50
+        
+        
+    def load(self, level):
+        geoMap="RCS/Maps/"+ level +".lvl"
+        thingMap="RCS/Maps/"+ level +".tng"
+
+        geofile = open(geoMap, "r")
+        lines = geofile.readlines()
+        geofile.close()
+        newlines = []
+		
+		#Clean up the file by stripping newlines!
+        for line in lines:
+            newline = ""
+            for character in line:
+                if character != "\n":
+                    newline += character
+            newlines += [newline]
+            
+        for y, line in enumerate(newlines):
+            for x, c in enumerate(line):
+                if c == "#":
+                    self.blocks += [Block([(x*10)+5, (y*10)+5], 
+                                           self.screenSize,
+                                           "rcs/imgs/block/cobblestone.png",
+                                           (10,10)
+                                           )]
+#-------Blocks
+                
+                    
+        #----Done with file---
+        
+        thingfile = open(thingMap, "r")
+        lines = thingfile.readlines()
+        thingfile.close() 
+        
+        newlines = []
+        
+        for line in lines:
+            newline = ""
+            for character in line:
+                if character != "\n":
+                    newline += character
+            newlines += [newline]
+            
+        for y, line in enumerate(newlines):
+            for x, c in enumerate(line):
+#-------Blocks
+                if c == "@":
+                    self.darkblocks += [Block([(x*10)+5, (y*10)+5], 
+                                            self.screenSize,
+                                            "rcs/imgs/block/spawnspace.png",
+                                            (10,10)
+                                            )]
+#-------Blocks
+#-------Blocks
+
+    
+#-------Blocks
