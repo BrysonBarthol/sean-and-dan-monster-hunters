00f0706098c7d93f68465e1dfc134f4a500b4f11
Bree <breanna.sowle@kearsarge.org>
2014-12-05 09:29:42 -0500

    fixed file names in .py files

--- Demons.py
+++ Demons.py
@@ -4,12 +4,12 @@ from Creature import Creature
 class Demon(Creature):
 	def __init__(self, pos):
 		Creature.__init__(self, image, speed = [0,0], pos = [0,0])
-		self.upImages = [pygame.image.load("RSC/Demon/dUp1.png"),
-							pygame.image.load("RSC/Demon/dUp2.png")]
-		self.downImages = [pygame.image.load("RSC/Demon/dDown1.png"),
-							pygame.image.load("RSC/Demon/dDown2.png")]
-		self.leftImages = [pygame.image.load("RSC/Demon/dLeft1.png"),
-							pygame.image.load("RSC/Demon/dLeft2.png")]
-		self.rightImages = [pygame.image.load("RSC/Demon/dRight1.png"),
-							pygame.image.load("RSC/Demon/dRight2.png")]]
+		self.upImages = [pygame.image.load("RSC/Demon/DemonUp1.png"),
+							pygame.image.load("RSC/Demon/DemonUp2.png")]
+		self.downImages = [pygame.image.load("RSC/Demon/DemonDown1.png"),
+							pygame.image.load("RSC/Demon/DemonDown2.png")]
+		self.leftImages = [pygame.image.load("RSC/Demon/DemonLeft1.png"),
+							pygame.image.load("RSC/Demon/DemonLeft2.png")]
+		self.rightImages = [pygame.image.load("RSC/Demon/DemonRight1.png"),
+							pygame.image.load("RSC/Demon/DemonRight2.png")]]
 		
