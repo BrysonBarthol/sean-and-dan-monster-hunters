585886ba1a83c2e14571848e624967187b82e920
Bryson Barthol <bryson.barthol@kearsarge.org>
2015-01-29 11:51:57 -0500

    Started Work on level save code ---SPOONER!!!

--- Game.py
+++ Game.py
@@ -28,75 +28,77 @@ level = Level("screen24", ["Dan", "Sean"], screenSize)
 players = level.players
 ghosts = level.ghosts
 
+run = True
 #ammo = HUDAmmo
 #coins = HUDCoins
 #ammo = Score([400, 25], "Ammo: ", 36)
 #coins = Score([600, 25], "Coins: ", 36)
 
 while True:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
-                if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_w or event.key == pygame.K_UP:
-                                players[0].go("up")
-                        if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                                players[0].go("right")
-                        if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                                players[0].go("down")
-                        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                                players[0].go("left")
-                        if event.key == pygame.K_SPACE:
-                                bullets += player.shoot()
-                if event.type == pygame.KEYUP:
-                        if event.key == pygame.K_w or event.key == pygame.K_UP:
-                                players[0].go("stop up")
-                        if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                                players[0].go("stop right")
-                        if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                                players[0].go("stop down")
-                        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                                players[0].go("stop left")
-                        if event.key == pygame.K_SPACE:
-                                player.shoot("stop")
-        
-        
-        for player in players:
-            player.update(screenWidth, screenHeight)
+        while run:
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT: sys.exit()
+                    if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                                    players[0].go("up")
+                            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                                    players[0].go("right")
+                            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                                    players[0].go("down")
+                            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                                    players[0].go("left")
+                            if event.key == pygame.K_SPACE:
+                                    bullets += player.shoot()
+                    if event.type == pygame.KEYUP:
+                            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                                    players[0].go("stop up")
+                            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                                    players[0].go("stop right")
+                            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                                    players[0].go("stop down")
+                            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                                    players[0].go("stop left")
+                            if event.key == pygame.K_SPACE:
+                                    player.shoot("stop")
             
-        for block in level.hardBlocks:
+            
             for player in players:
-                if block.playerCollide(player):
-                    player.go("stop")
-        
-        for levelChangeBlock in level.levelChangeBlocks:
-            #print levelChangeBlock.newlev
-            for player in players:
-                if levelChangeBlock.playerCollide(player):
-                    print "new level"
-                    level.load(levelChangeBlock.newlev, levelChangeBlock.kind)
-        
-        for bullet in bullets:
-            bullet.update(screenWidth, screenHeight)
-        
-        print len(bullets)        
-        for bullet in bullets:
-            if not bullet.living:
-                bullets.remove(bullet)
-                    
+                player.update(screenWidth, screenHeight)
+                
+            for block in level.hardBlocks:
+                for player in players:
+                    if block.playerCollide(player):
+                        player.go("stop")
+            
+            for levelChangeBlock in level.levelChangeBlocks:
+                #print levelChangeBlock.newlev
+                for player in players:
+                    if levelChangeBlock.playerCollide(player):
+                        print "new level"
+                        level.load(levelChangeBlock.newlev, levelChangeBlock.kind)
+            
+            for bullet in bullets:
+                bullet.update(screenWidth, screenHeight)
+            
+            print len(bullets)        
+            for bullet in bullets:
+                if not bullet.living:
+                    bullets.remove(bullet)
+                        
 
-        red = 0
-        green = 0
-        blue = 0
-        bgColor = red, green, blue
-        screen.fill(bgColor)
-        for block in level.blocks:
-            screen.blit(block.image, block.rect)
-        for levelChangeBlock in level.levelChangeBlocks:
-            screen.blit(levelChangeBlock.image, levelChangeBlock.rect)
-        for player in players:
-            screen.blit(player.image, player.rect)
-        for bullet in bullets:
-            screen.blit(bullet.image, bullet.rect)
-        for ghost in ghosts:
-            screen.blit(ghost.image, ghost.rect)
-        pygame.display.flip()
+            red = 0
+            green = 0
+            blue = 0
+            bgColor = red, green, blue
+            screen.fill(bgColor)
+            for block in level.blocks:
+                screen.blit(block.image, block.rect)
+            for levelChangeBlock in level.levelChangeBlocks:
+                screen.blit(levelChangeBlock.image, levelChangeBlock.rect)
+            for player in players:
+                screen.blit(player.image, player.rect)
+            for bullet in bullets:
+                screen.blit(bullet.image, bullet.rect)
+            for ghost in ghosts:
+                screen.blit(ghost.image, ghost.rect)
+            pygame.display.flip()
--- Level.py
+++ Level.py
@@ -12,17 +12,43 @@ class Level():
         self.screenHeight = screenSize[1]
         self.blocks = []
         self.hardBlocks = []
+        
         self.levelChangeBlocks = []
         self.ghosts = []
+        
         self.players = []
+        
         self.blockSize = 50
         self.level = level
         self.load(level)
 
         
-
-
-    def load(self, level, source=None):
+    def unload(self):
+        things = []
+        line = []
+        for y in range(14):
+            for x in range(20):
+                line += [" "]
+            things += [line]
+            line = []
+        #print len(things), len(things[0])
+        
+        for ghost in self.ghosts:
+            things[ghost.rect.center[1]/50][ghost.rect.center[1]/50] = "G"
+        for lc in self.levelChangeBlocks:
+            things[lc.rect.center[1]/50][lc.rect.center[1]/50] = lc.kind
+        
+        thingString = ""
+        for line in things:
+            for c in line:
+                thingString += c
+            thingString += "\n"
+        print thingString
+        
+        thingMap="RSC/Maps/"+ self.level +".tngs"
+        savedThingfile = open(thingMap, "w")
+        savedThingfile.write(thingString)
+            
         while len(self.blocks) > 0:
             self.blocks.remove(self.blocks[0])
         while len(self.hardBlocks) > 0:
@@ -31,8 +57,9 @@ class Level():
             self.levelChangeBlocks.remove(self.levelChangeBlocks[0])
         while len(self.ghosts) > 0:
             self.ghosts.remove(self.ghosts[0])
-        
-            
+
+    def load(self, level, source=None):  
+        self.unload()          
         self.level = level
         print self.level
         geoMap="RSC/Maps/"+ level +".lvl"
