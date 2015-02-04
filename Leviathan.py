f05058b131377e4fa5f07f51349af469b6978520
Bree <breanna.sowle@kearsarge.org>
2015-02-04 09:01:22 -0500

    wyhgscjaswcxd

--- Leviathan.py
+++ Leviathan.py
@@ -8,8 +8,7 @@ class Leviathan(Demon):
     def __init__(self, pos):
         image = ("RSC/Leviathan/LeviUp1.png")
         speed = [1,0]
-        self.seen = False
-        Demon.__init__(self, pos)
+        Demon.__init__(self, "RSC/Leviathan/LeviDown1.png", speed, pos)
         self.upImages = [pygame.image.load("RSC/Leviathan/LeviUp1.png"),
                             pygame.image.load("RSC/Leviathan/LeviUp2.png")]
         self.downImages = [pygame.image.load("RSC/Leviathan/LeviDown1.png"),
@@ -17,23 +16,24 @@ class Leviathan(Demon):
         self.leftImages = [pygame.image.load("RSC/Leviathan/LeviLeft1.png"),
                             pygame.image.load("RSC/Leviathan/LeviLeft2.png")]
         self.rightImages = [pygame.image.load("RSC/Leviathan/LeviRight1.png"),
-                            pygame.image.load("RSC/Leviathan/LeviRight2.png")]
-                            
+                            pygame.image.load("RSC/Leviathan/LeviRight2.png")]                   
+        self.images = self.downImages
+        self.image = self.images[self.frame]
+    
     def move(self):
         self.speed = [self.speedx, self.speedy]
         self.rect = self.rect.move(self.speed)
     
     def shoot(self):
-        return Bullet(self.rect.center, self.facing) 
+        return Bullet(self.rect.center, self.facing)
         self.changed = True
         self.shooting = True
                             
     def detectPlayer(self, player):
-        if self.seen == True:
+        if seen:
             if xdiff > 0: #to the right of the player
                 self.speed = 0
                 self.facing = "right"
-                
             elif xdiff < 0: #to the left
                 self.speed = 0
                 self.facing = "left"
