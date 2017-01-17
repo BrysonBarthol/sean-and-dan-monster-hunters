c0fc24ed09de07dbbd0c84f7837c33dd8f4e9308
Chris Spooner <cspooner@kearsarge.org>
2017-01-17 15:16:36 -0500

    World builder work

--- RSC/Maps/WorldBuilder.py
+++ RSC/Maps/WorldBuilder.py
@@ -2,17 +2,55 @@ import os
 
 files = os.listdir("./")
 
-w1 = []
-w2 = []
-w3 = []
+worlds = [[],[],[]]
 for f in files:
     if f[-4:] == ".tng":
         if f[6] == "1":
-            w1 += [f]
+            worlds[0] += [f]
         elif f[6] == "2":
-            w2 += [f]
+            worlds[1] += [f]
         elif f[6] == "3":
-            w3 += [f]
+            worlds[2] += [f]
 
-files = newFiles
-print files
+for world in worlds:
+    for f in world:
+        print f
+    print "----------"
+    
+
+for world in worlds:
+    maxX = 0
+    maxY = 0
+
+    for f in world:
+        if int(f[7]) > maxX:
+            maxX = int(f[7])
+        if int(f[8]) > maxY:
+            maxY= int(f[8])
+
+
+
+    print maxX, maxY
+
+
+""""
+ 
+wMap1 = " "
+
+for x in range(maxX1):
+    wMap1 += str(x+1)*20 + '|'
+    
+wMap1 += "\n"
+
+for y in range(maxY1):
+    line = str(y+1)
+    for x in range(maxX1):
+        line += " "*20 + '|'
+    line += '\n'
+    wMap1 += line * 15 
+    wMap1 += "-" * 21 * maxX1 + "-" + '\n'
+    
+outFile = open("wMap1.txt", 'w')
+outFile.write(wMap1)
+outFile.close()
+"""
