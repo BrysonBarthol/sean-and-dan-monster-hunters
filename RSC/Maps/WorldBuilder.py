7681b364fd687bfa45e71419a7b3cf9deef8120c
Chris Spooner <cspooner@kearsarge.org>
2017-01-18 18:14:18 -0500

    Maps are not quite right....

--- RSC/Maps/WorldBuilder.py
+++ RSC/Maps/WorldBuilder.py
@@ -28,39 +28,48 @@ for w, world in enumerate(worlds):
         if int(f[8]) > maxY:
             maxY= int(f[8])
 
-
-
-    print maxX, maxY
-
-
-
- 
-    wMap = [[]]
+    #print maxX, maxY
+    wMap = [[" "]]
 
     for x in range(maxX):
         wMap[0] += [str(x+1)*20] + ['|']
     
-
-
     for y in range(maxY):
         line = [str(y+1)]
         for x in range(maxX):
             line += [" "*20] + ['|']
-            print line
         for i in range(15):
             wMap += [line]
-            print len(wMap), len(wMap[-1])
         wMap += ["-" * 21 * maxX + "-"]
         
+    for theWorld in world:
+        f = open(theWorld, 'r')
+        lines = f.readlines()
+        f.close()
         
+        thisX = (int(theWorld[7])-1) * 2 + 1
+        thisY = (int(theWorld[8])-1) * 16 + 1
+        
+        print theWorld
+        print thisX, thisY
+        for al, theline in enumerate(lines):
+            wMap[thisY+al][thisX] = theline[:-1]
+            
+        print wMap
     
- 
+    print wMap
+
+
+
+
     out = ""
     for l in wMap:
         for s in l:
+            #print s
             out += s
         out += '\n'
         
+    print out
 
     outFile = open("wMap" + str(w+1) + ".txt", 'w')
     outFile.write(out)
